48204fc094256dd90152bd2b9ad50d320cb31fcb
Jason Peters <jpeters1228@comcast.net>
2019-02-14 22:40:30 -0700

    Added haptics on button press

--- app/src/main/java/com/shelatok/diceroller2/MainActivity.kt
+++ app/src/main/java/com/shelatok/diceroller2/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.shelatok.diceroller2
 
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +13,14 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     fun buttonClick(view: View){
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (vibrator.hasVibrator()) { // Vibrator availability checking
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)) // New vibrate method for API Level 26 or higher
+            } else {
+                vibrator.vibrate(100) // Vibrate method for below API Level 26
+            }
+        }
 
 //        for (i in 1..100000) {
 //            dieRoll.text = i.toString()
